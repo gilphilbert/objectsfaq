b712216020298e6bfc2a0ae1270ec2fda79eed26
gilphilbert <gil.philbert@gmail.com>
2021-04-27 10:24:29 -0700

    correct locations for icons

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
 const cacheName = 'faqs-v1'
 const cacheFiles = [
-  '/index.html',
-  '/faqs.json',
-  '/favicon.ico',
+  '/objectsfaq/index.html',
+  '/objectsfaq/faqs.json',
+  '/objectsfaq/favicon.ico',
+  '/objectsfaq/icon-16x16.png',
+  '/objectsfaq/icon-32x32.png',
+  '/objectsfaq/icon-192x192.png',
+  '/objectsfaq/icon-256x256.png',
+  '/objectsfaq/icon-384x384.png',
+  '/objectsfaq/icon-512x512.png',
 
   'https://cdn.jsdelivr.net/npm/vue@2.6.12',
   'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
   'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css',
+
+  'https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js'
 ]
 
 self.addEventListener('install', evt => {
